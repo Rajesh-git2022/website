a6b7fc6edbd10f88b50bcf1f6346194308886333
Ashutosh Sharma <ashutoshshrma21photos@gmail.com>
2022-07-17 00:51:37 +0530

    Updates

---
+++ precache-manifest.abd509860c76d47db00490c2df6ed13b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af82b7b5300f031bff46",
+    "url": "/website/static/css/main.c2ad2004.chunk.css"
+  },
+  {
+    "revision": "af82b7b5300f031bff46",
+    "url": "/website/static/js/main.0c9de289.chunk.js"
+  },
+  {
+    "revision": "bc518842e8e7a41bc1d0",
+    "url": "/website/static/js/runtime~main.ad17642f.js"
+  },
+  {
+    "revision": "23ec104f03e55d5e1f9e",
+    "url": "/website/static/js/2.2d72ab8d.chunk.js"
+  },
+  {
+    "revision": "6eed086804cab7e3b6d1",
+    "url": "/website/static/js/3.eac466b7.chunk.js"
+  },
+  {
+    "revision": "6ad2787d6c26686f6ab3df2e4d3a1576",
+    "url": "/website/index.html"
+  }
+];
